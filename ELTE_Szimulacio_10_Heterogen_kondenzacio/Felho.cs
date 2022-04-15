59ef619e38e3ba696f64c26def87460e081a4e22
Molnár Attila <molnar.attila@szlgbp.hu>
2022-04-15 20:00:16 +0200

    alapok

---
+++ ELTE_Szimulacio_10_Heterogen_kondenzacio/Felho.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace ELTE_Szimulacio_10_Heterogen_kondenzacio
+{
+	class Felho
+	{
+		private int N;
+		private int M;
+		private decimal BV;
+		private decimal BK;
+		private int T;
+		private Bitmap bmp;
+		private PictureBox pb;
+		private int[,] Tér;
+		private Random r;
+
+		public Felho(int N, int M, decimal BV, decimal BK, int T, PictureBox pb)
+		{
+			this.N = N;
+			this.M = M;
+			this.BV = BV;
+			this.BK = BK;
+			this.T = T;
+			this.pb = pb;
+			this.pb.Size = new Size(N, M);
+			this.bmp = new Bitmap(N,M);
+			this.pb.Image = bmp;
+			this.Tér = new int[N,M];
+			this.r = new Random();
+		}
+		public void Lépés()
+		{
+			Point P = Véletlen_hely();
+			Point Q = Véletlen_szomszéd_fel(P);
+
+			if (P.X == N+1)
+			{
+				Belépés(Q);
+			}
+			else if (P.X == 1)
+			{
+				Kilépés(P);
+			}
+			else
+			{
+				Mozgás(P, Q);
+			}
+		}
+
+		private void Belépés(Point q)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void Kilépés(Point p)
+		{
+			throw new NotImplementedException();
+		}
+
+
+		private void Mozgás(Point p, Point q)
+		{
+			throw new NotImplementedException();
+		}
+
+		private Point Véletlen_szomszéd_fel(Point p)
+		{
+			throw new NotImplementedException();
+		}
+
+		private Point Véletlen_hely()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- ELTE_Szimulacio_10_Heterogen_kondenzacio/Form1.cs
+++ ELTE_Szimulacio_10_Heterogen_kondenzacio/Form1.cs
@@ -12,10 +12,20 @@ namespace ELTE_Szimulacio_10_Heterogen_kondenzacio
 {
 	public partial class Form1 : Form
 	{
+		Felho f;
 		public Form1()
 		{
 			InitializeComponent();
 		}
 
+		private void btn_uj_Click(object sender, EventArgs e)
+		{
+			int N = (int)nud_N.Value;
+			int M = (int)nud_M.Value;
+			decimal BV = nud_BV.Value;
+			decimal BK = nud_BK.Value;
+			int T = (int)nud_T.Value;
+			f = new Felho(N, M, BV, BK, T, pb);
+		}
 	}
 }
